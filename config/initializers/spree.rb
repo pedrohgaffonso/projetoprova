c74b0e0217e4d24e83af8c97c69059e47ff3b53c
pedrohgaffonso <pedro.affonso@gfatec.sp.gov.br>
2018-11-28 10:33:56 -0200

    logo alterado

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -22,7 +22,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus_logo.png"
+   config.logo = "logo/logo.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -31,7 +31,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus_logo.png"
+   config.admin_interface_logo = "logo/logo.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
